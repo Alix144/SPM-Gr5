2c5161c632fce908484bd6f65d8bf75673c9ff83
Ali <86134214+Alix144@users.noreply.github.com>
2023-05-01 10:52:51 +0300

    Student page completed

---
+++ spm/webpages/index.js
@@ -0,0 +1,10 @@
+let announcementBtn = document.getElementsByClassName('announcement-btn');
+let jobBtn = document.getElementsByClassName('job-btn');
+
+function navigateAnnouncement(){
+    window.location.href = "announcements.html";
+}
+
+function navigateJobs(){
+    window.location.href = "jobs.html";
+}
